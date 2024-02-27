128a3fba976ed72b548293fdedf938076b2ed507
Sandeep Virdi <code.monkey.1982@gmail.com>
2024-02-26 21:00:40 -0800

    Adding frame crate
    Framing logic for simple protocol
    
    Adding get_line, get_number and get_u8 functions
    
    moving things around
    
    Adding StrData type for parsing
    
    testing commit signing
    
    Signed-off-by: Sandeep Virdi <code.monkey.1982@gmail.com>
    
    removing old protocol comments
    
    Signed-off-by: Sandeep Virdi <code.monkey.1982@gmail.com>

---
+++ src/frame.rs
@@ -0,0 +1,108 @@
+// TODO: Add more details around the protocol
+// Keep the design simple.
+
+// Protocol Design
+// Lets create a new protocol with
+// client / server calculator
+//
+
+use std::{io::Cursor, u64};
+
+use tokio_util::bytes::Buf;
+
+// A frame for our own protocol.
+#[derive(Clone, Debug)]
+pub enum Frame {
+    Integer(u64),
+    StrData(String),
+    Array(Vec<Frame>),
+}
+
+#[derive(Debug)]
+pub enum Error {
+    // Not enough data is available
+    Incomplete,
+
+    // Non supported encoding.
+    ErrMessage(String),
+}
+
+impl Frame {
+    pub fn array() -> Frame {
+        Frame::Array(vec![])
+    }
+
+    pub fn check(src: &mut Cursor<&[u8]>) -> Result<(), Error> {
+        match get_u8(src)? {
+            b'%' => {
+                // free form bytes
+                get_line(src)?;
+                Ok(())
+            }
+            b':' => {
+                let _ = get_number(src)?;
+                Ok(())
+            }
+            default => Err(Error::ErrMessage(format!(
+                "protocol error, invalid type byte {}",
+                default
+            ))),
+        }
+    }
+
+    pub fn parse(src: &mut Cursor<&[u8]>) -> Result<Frame, Error> {
+        match get_u8(src)? {
+            b':' => {
+                let num = get_number(src)?;
+                Ok(Frame::Integer(num))
+            }
+            b'%' => {
+                let line = get_line(src)?.to_vec();
+                let string = String::from_utf8(line)
+                    .map_err(|_| Error::ErrMessage("Failed to parse the string".to_string()))?;
+                Ok(Frame::StrData(string))
+            }
+            _ => !unimplemented!(),
+        }
+    }
+}
+
+fn get_u8(src: &mut Cursor<&[u8]>) -> Result<u8, Error> {
+    if !src.has_remaining() {
+        return Err(Error::Incomplete);
+    }
+    Ok(src.get_u8())
+}
+
+// Get Number
+fn get_number(src: &mut Cursor<&[u8]>) -> Result<u64, Error> {
+    use atoi::atoi;
+
+    let line = get_line(src)?;
+
+    atoi::<u64>(line).map_or(
+        Err(Error::ErrMessage(
+            "Protocol error, invalid frame".to_string(),
+        )),
+        |v| Ok(v),
+    )
+}
+
+// Find line terminating character = `<` `>`
+fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8], Error> {
+    let start = src.position() as usize;
+
+    // last byte position
+    let end = src.get_ref().len() - 1;
+
+    // Search for the termination pattern
+    for i in start..end {
+        if src.get_ref()[i] == b'<' && src.get_ref()[i + 1] == b'>' {
+            // update the position after `>`
+            src.set_position((i + 2) as u64);
+
+            return Ok(&src.get_ref()[start..i]);
+        }
+    }
+    Err(Error::Incomplete)
+}
